6c070ae12f64ce4a08a9f3607a04dbe18a4af23e
Andrew Son <as10443@nyu.edu>
2019-11-07 21:32:34 -0500

    SDLTilemap

---
+++ SDLTileMap/SDLProject/Level3.cpp
@@ -0,0 +1,9 @@
+//
+//  Level3.cpp
+//  SDLProject
+//
+//  Created by Andrew Son on 10/31/19.
+//  Copyright © 2019 ctg. All rights reserved.
+//
+
+#include "Level3.hpp"
---
+++ SDLTileMap/SDLProject/Level3.h
@@ -0,0 +1,14 @@
+//
+//  Level3.hpp
+//  SDLProject
+//
+//  Created by Andrew Son on 10/31/19.
+//  Copyright © 2019 ctg. All rights reserved.
+//
+
+#ifndef Level3_hpp
+#define Level3_hpp
+
+#include <stdio.h>
+
+#endif /* Level3_hpp */
