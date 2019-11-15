042930db5c22c2722d95d5e3780ab877a5925703
Andrew Son <as10443@nyu.edu>
2019-11-15 12:14:42 -0500

    Menu Included

---
+++ SDLTileMap/SDLProject/menu.cpp
@@ -0,0 +1,9 @@
+//
+//  menu.cpp
+//  SDLProject
+//
+//  Created by Andrew Son on 11/15/19.
+//  Copyright © 2019 ctg. All rights reserved.
+//
+
+#include "menu.hpp"
---
+++ SDLTileMap/SDLProject/menu.h
@@ -0,0 +1,14 @@
+//
+//  menu.hpp
+//  SDLProject
+//
+//  Created by Andrew Son on 11/15/19.
+//  Copyright © 2019 ctg. All rights reserved.
+//
+
+#ifndef menu_hpp
+#define menu_hpp
+
+#include <stdio.h>
+
+#endif /* menu_hpp */
